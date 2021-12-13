0092a4beaf87d4cefd1dc3d1e70f31a1d206a319
Renato Welinton Schlogel <renato.s@outlook.com>
2021-12-13 17:33:14 -0300

    criando o projeto

---
+++ src/test/java/com/renatoschlogel/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatoschlogel.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
